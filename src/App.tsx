7e6d34914ff8dee5718e9aa3037c9b4cb4abb22b
thirumurugan7 <thiru82003@gmail.com>
2022-11-22 15:31:50 +0530

    added backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const clientId = "<client_id_from_the_web3auth_dashboard_plug_n_play_project>";
 
-export default App;
+const openloginAdapter = new OpenloginAdapter({
+  adapterSettings: {
+    clientId,
+    network: "testnet",
+    uxMode: "popup",
+    whiteLabel: {
+      name: "Twitter DApp",
+      logoLight: "<hosted-logo-image-link>",
+      logoDark: "<hosted-logo-image-link>",
+      defaultLanguage: "en",
+      dark: true, // whether to enable dark mode. defaultValue: false
+    },
+    loginConfig: {
+      // Add login configs corresponding to the providers on modal
+      // Twitter login
+      jwt: {
+        name: "Custom Auth Login",
+        Verifier: "<client_verifier_from_Auth0_dashboard>", // Please create a verifier on the developer dashboard and pass the name here
+        typeOfLogin: "twitter", // Pass on the login provider of the verifier you've created
+        clientId: "<clientId_from_Auth_0_dashboard>", // Pass on the clientId of the login provider here - Please note this differs from the Web3Auth ClientID. This is the JWT Client ID
+      },
+    },
+  },
+});
+
+web3auth.configureAdapter(openloginAdapter);
